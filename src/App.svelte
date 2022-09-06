d53bffe2b7aafbf9e9df349af08d08fd21095c8c
Mnigos <makowskiigor@gmail.com>
2022-09-06 14:11:52 +0200

    feat(lib): create `Footer`

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
   import Button from './lib/Button.svelte'
+  import Footer from './lib/Footer.svelte'
 </script>
 
 <main
-  class="bg-dark-800 min-h-screen text-white flex flex-col items-center justify-center"
+  class="bg-dark-800 min-h-screen text-white grid justify-items-center grid-rows-2"
 >
-  <header class="text-center p-4">
-    <h1 class="text-5xl p-2">Voting App</h1>
+  <div class="flex flex-col items-center gap-8">
+    <header class="text-center p-4">
+      <h1 class="text-5xl p-2">Voting App</h1>
 
-    <p>Connect your metamask wallet to get involved into voting</p>
-  </header>
+      <p>Connect your metamask wallet to get involved into voting</p>
+    </header>
 
-  <div class="card text-gray-500">
     <Button>Connect wallet</Button>
+  </div>
+
+  <div class="self-end">
+    <Footer />
   </div>
 </main>
 
---
+++ src/lib/Footer.svelte
@@ -0,0 +1,8 @@
+<div class="w-full">
+  <div
+    class="p-4 border-2 border-b-0 border-purple-800"
+    style="border-top-left-radius: 1.5rem; border-top-right-radius: 1.5rem;"
+  >
+    Igor Makowski {new Date().getFullYear()} &copy; All rights reserved
+  </div>
+</div>
