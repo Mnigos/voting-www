c6829613485c575011c52b0d61659f718e236530
Mnigos <makowskiigor@gmail.com>
2022-09-08 17:39:39 +0200

    feat(lib/button): add green color and `isFilled` property

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -4,15 +4,25 @@
   export let color: Colors = Colors.PURPLE
   export let isFullRounded = false
   export let noPadding = false
+  export let isFilled = false
 
   const colorClasses = {
-    [Colors.PURPLE]: 'border-purple-800 text-purple-800 hover:bg-purple-800',
-    [Colors.DARK]: 'border-dark-800 text-dark-800 hover:bg-dark-800',
+    [Colors.PURPLE]: `border-purple-800 ${
+      isFilled
+        ? 'bg-purple-800 text-white'
+        : 'hover:bg-purple-800 text-purple-800'
+    }`,
+    [Colors.DARK]: `border-dark-800 ${
+      isFilled ? 'bg-dark-800 text-white' : 'hover:bg-dark-800 text-dark-800'
+    }`,
+    [Colors.GREEN]: `border-green-800 ${
+      isFilled ? 'bg-green-800 text-white' : 'hover:bg-green-800 text-green-800'
+    }`,
   }
 </script>
 
 <button
-  class={`border-2 transition-all duration-300 hover:text-white transform active:rotate-12 active:shadow-xl px-4 py-2 ${colorClasses[color]}`}
+  class={`border-2 transition-all duration-300 hover:text-white text-white transform active:rotate-12 active:shadow-xl px-4 py-2 ${colorClasses[color]} ${$$restProps.class}`}
   class:px-4={!noPadding}
   class:py-2={!noPadding}
   class:rounded-full={isFullRounded}
