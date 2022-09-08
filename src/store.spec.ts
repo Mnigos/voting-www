aef7158c69e2b2d1b99996de3984b7197421e812
Mnigos <makowskiigor@gmail.com>
2022-09-08 15:55:58 +0200

    feat(store): add `setProvider` and `setAccount` methods

--- src/store.spec.ts
+++ src/store.spec.ts
@@ -1,14 +1,43 @@
 import { get } from 'svelte/store'
-import { describe, expect, test } from 'vitest'
+import { describe, expect, test, vi } from 'vitest'
+import { generateTestingUtils } from 'eth-testing'
 
-import { account } from './store'
+import {
+  account,
+  setProvider,
+  provider,
+  setAccount,
+  showNoEthereumAlert,
+} from './store'
 
 describe('store', () => {
-  test('account', () => {
-    expect(get(account)).toEqual('')
+  const ethersTestUtils = generateTestingUtils({ providerType: 'MetaMask' })
+  const testAccount = '0xf61B443A155b07D2b2cAeA2d99715dC84E839EEf'
+  const providerMock = {
+    ...ethersTestUtils.getProvider(),
+    request: vi.fn().mockImplementation(() => [testAccount]),
+  }
 
-    account.set('foo')
+  test('setProvider', async () => {
+    setProvider(providerMock)
 
-    expect(get(account)).toEqual('foo')
+    const [mockAccount] = await get(provider).request({ method: '' })
+
+    expect(mockAccount).toEqual(testAccount)
+  })
+
+  test('setAccount', async () => {
+    await setProvider(providerMock)
+    await setAccount()
+
+    expect(await get(account)).toEqual(testAccount)
+  })
+
+  test('showNoEthereumAlert', () => {
+    expect(get(showNoEthereumAlert)).toEqual(false)
+
+    showNoEthereumAlert.set(true)
+
+    expect(get(showNoEthereumAlert)).toEqual(true)
   })
 })
