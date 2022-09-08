a4c5f7e85317b1136117b0948c2e92b406d07d85
Mnigos <makowskiigor@gmail.com>
2022-09-08 15:55:58 +0200

    feat(store): add `setProvider` and `setAccount` methods

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,24 @@
-import { writable } from 'svelte/store'
+import { get, writable } from 'svelte/store'
+import { providers } from 'ethers'
 
-export const account = writable<string>('')
+export const account = writable<string>()
+export const provider = writable<providers.ExternalProvider>()
 export const showNoEthereumAlert = writable<boolean>(false)
+
+export function setProvider(
+  newProvider: providers.ExternalProvider | providers.JsonRpcFetchFunc
+) {
+  const web3Provider = new providers.Web3Provider(newProvider)
+
+  provider.set(web3Provider.provider)
+}
+
+export async function setAccount() {
+  const currentProvider = get(provider)
+
+  const [currentAccount]: string = await currentProvider.request({
+    method: 'eth_requestAccounts',
+  })
+
+  account.set(currentAccount)
+}
