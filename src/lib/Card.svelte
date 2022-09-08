ae96f6170171465ee78c651483ac4c87f9568c2c
Mnigos <makowskiigor@gmail.com>
2022-09-08 17:39:18 +0200

    feat(lib): create `Card`

---
+++ src/lib/Card.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let title: string
+</script>
+
+<div class="bg-dark-600 p-4 rounded-xl flex flex-col items-center gap-5">
+  <header class="text-2xl">
+    <h3>{title}</h3>
+  </header>
+
+  <main>
+    <slot />
+  </main>
+</div>
