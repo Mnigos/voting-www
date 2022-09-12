1b1de82e1adde5b479f30173bec72db872642c17
Mnigos <makowskiigor@gmail.com>
2022-09-12 18:35:37 +0200

    refactor(StatsCard): implement new sotre functions

--- src/lib/StatsCard.svelte
+++ src/lib/StatsCard.svelte
@@ -5,10 +5,9 @@
 
   import {
     avaibleVotes,
-    account,
     isVoted,
     yourVote,
-    addVoter,
+    signInForVoting,
     getStats,
   } from '~/store'
 
@@ -17,31 +16,25 @@
   })
 </script>
 
-{#if $account}
-  <div class="flex flex-col gap-4">
-    <div
-      class="bg-dark-600 p-2 rounded-lg shadow-xl flex gap-2 justify-between"
-    >
-      <p class="text-gray-400">Avaible votes:</p>
-      <div class="rounded-lg text-center bg-gray-600 px-2">{$avaibleVotes}</div>
-    </div>
-
-    <div
-      class="bg-dark-600 p-2 rounded-lg shadow-xl flex gap-2 justify-between"
-    >
-      {#if $isVoted}
-        <p class="text-gray-400">You have voted for:</p>
-
-        <div class="rounded-lg text-center bg-gray-600 px-2">{$yourVote}</div>
-      {:else}
-        <span class="flex flex-col gap-2">
-          <p class="text-gray-400">You haven't voted yet</p>
-
-          {#if !$avaibleVotes}
-            <Button on:click={addVoter}>Sign In for voting</Button>
-          {/if}
-        </span>
-      {/if}
-    </div>
+<div class="flex flex-col gap-4">
+  <div class="bg-dark-600 p-2 rounded-lg shadow-xl flex gap-2 justify-between">
+    <p class="text-gray-400">Avaible votes:</p>
+    <div class="rounded-lg text-center bg-gray-600 px-2">{$avaibleVotes}</div>
   </div>
-{/if}
+
+  <div class="bg-dark-600 p-2 rounded-lg shadow-xl flex gap-2 justify-between">
+    {#if $isVoted}
+      <p class="text-gray-400">You have voted for:</p>
+
+      <div class="rounded-lg text-center bg-gray-600 px-2">{$yourVote}</div>
+    {:else}
+      <span class="flex flex-col gap-2">
+        <p class="text-gray-400">You haven't voted yet</p>
+
+        {#if !$avaibleVotes}
+          <Button on:click={signInForVoting}>Sign In for voting</Button>
+        {/if}
+      </span>
+    {/if}
+  </div>
+</div>
