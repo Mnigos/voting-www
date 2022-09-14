41176109b46601c4f9e04494d38d4628d3a84413
Mnigos <makowskiigor@gmail.com>
2022-09-14 12:24:07 +0200

    feat(lib): create `ProposalForm`

---
+++ src/lib/ProposalForm.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Button from './Button.svelte'
+  import Input from './Input.svelte'
+
+  import { addProposal } from '~/store'
+
+  let proposalName: string
+
+  async function submit() {
+    if (!proposalName) return
+
+    await addProposal(proposalName)
+  }
+</script>
+
+<div class="bg-dark-600 rounded-lg p-4 flex flex-col gap-6">
+  <header class="text-2xl">
+    <h3>Add new proposal</h3>
+  </header>
+
+  <form class="flex flex-col gap-3" on:submit|preventDefault={submit}>
+    <Input label="Name" bind:value={proposalName} />
+
+    <Button>Submit</Button>
+  </form>
+</div>
