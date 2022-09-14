1dd2c2ad3206343bddd53518b980f4863e6c9c9b
Mnigos <makowskiigor@gmail.com>
2022-09-14 12:23:49 +0200

    feat(lib): create `Input`

---
+++ src/lib/Input.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let label: string
+  export let value: string
+</script>
+
+<label class="flex flex-col group">
+  <p
+    class="ml-1 text-gray-600 transform group-hover:-translate-x-16 group-hover:opacity-0 opacity-100 transition-all duration-300"
+  >
+    {label}
+  </p>
+  <input
+    type="text"
+    bind:value
+    class="bg-dark-600 border-2 border-purple-800 rounded-xl focus:outline-none focus:ring p-2 focus:ring-purple-800 focus:border-purple-800"
+  />
+</label>
